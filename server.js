4dc554dbdcc545c044842b55aabc2fe4ee201f84
Philip Ogboh <philipogboh60@gmail.com>
2024-11-06 08:20:52 +0100

    CORS issue fix

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://music-player-beta-azure.vercel.app",
+    ],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
   })
